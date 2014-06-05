5bf352d128082f0dfd90f7fd04e48743dd24b498
Shreyas <shrykulk.rk85@gmail.com>
2014-06-06 00:34:30 +0530

    Final Blog Site ready.

--- flaskr.py
+++ flaskr.py
@@ -49,6 +49,42 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries(title, text) values (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New Entry added successfully!')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid User!'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Password!'
+        else:
+            session['logged_in'] = True
+            flash('You\'re now logged in!')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You\'re now logged out!')
+    return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
     app.run()
